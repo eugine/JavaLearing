e01bae41102aca14e29b3bf91b84ffaeb0d3fb24
Eugene Sokolov <eugine_s@ukr.net>
2014-02-10 22:08:41 +0200

    Initial version added.

---
+++ src/test/java/ua/eugenesokolov/AppTest.java
@@ -0,0 +1,12 @@
+package ua.eugenesokolov;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest
+{
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
